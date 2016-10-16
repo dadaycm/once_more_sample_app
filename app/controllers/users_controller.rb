eef1024f5b319fb7fc5c6b53a7fb639432d89d74
Hongying LIU <dadaycm@hotmail.com>
2016-10-16 20:16:15 +0800

    chapter 8 Sessions, login, logout;flash now;test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,18 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    debugger if Rails.env.development?
+    #debugger if Rails.env.development?
   end
 
   def new
     @user = User.new
-    debugger  if Rails.env.development?
+    #debugger  if Rails.env.development?
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
