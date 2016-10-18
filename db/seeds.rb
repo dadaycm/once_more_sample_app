3be94d25b64372e5a828f206dab4bb8bb1c4986c
Hongying LIU <dadaycm@hotmail.com>
2016-10-18 22:17:15 +0800

    modify seed to set myself as admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,18 @@
+User.delete_all()
+
+User.create!(name:  "dadaycm",
+             email: "dadaycm@hotmail.com",
+             password:              "Adam2015",
+             password_confirmation: "Adam2015",
+             admin: true)
+
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true)
 
-99.times do |n|
+20.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
