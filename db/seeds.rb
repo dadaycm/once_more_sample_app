0c279bb849b0f72bc5170631b790cf559b09b8e5
Hongying LIU <dadaycm@hotmail.com>
2016-10-19 22:20:19 +0800

    13.2 rake test pass

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,3 +35,11 @@ User.create!(name:  "Example User",
               activated: true,
               activated_at: Time.zone.now)
 end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  # content = Faker::Hipster.sentences(1)
+  # content = Faker::ChuckNorris.fact
+  users.each { |user| user.microposts.create!(content: content) }
+end
