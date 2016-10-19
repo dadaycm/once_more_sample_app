ecfdd3ec15edaaf488935df4fe6809da03ea400d
Hongying LIU <dadaycm@hotmail.com>
2016-10-19 08:25:07 +0800

    11.1-user.rb changed, seed, yaml changed;test pass

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,20 +4,26 @@ User.create!(name:  "dadaycm",
              email: "dadaycm@hotmail.com",
              password:              "Adam2015",
              password_confirmation: "Adam2015",
-             admin: true)
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 20.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
