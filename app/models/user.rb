4ca7e7712e2dc4bde05f01cf640a76c842d7dad3
Hongying LIU <dadaycm@hotmail.com>
2016-10-19 22:42:14 +0800

    13.3 all except picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -69,6 +69,13 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
+
   self.per_page = 6
 
   private
