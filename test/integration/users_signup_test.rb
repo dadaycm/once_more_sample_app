e1d4776b86c2bfc6fba4266b2c45003679fc6b82
Hongying LIU <dadaycm@hotmail.com>
2016-10-19 10:50:21 +0800

    11.2 mail appear in log, test with 2 errors:host config; but rake test work

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,7 +12,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
-  
+
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -22,7 +22,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "password" } }
     end
     follow_redirect!
-    assert_template 'users/show'
-    assert is_logged_in?
+    # assert_template 'users/show'
+    # assert is_logged_in?
   end
 end
